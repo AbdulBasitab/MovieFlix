3b4a58866b8560c5b80ce0b07fc023d9ad7d78dc
Abdul Basit <abdulbasit.appstree@gmail.com>
2023-07-22 11:54:44 +0500

    refactored files and code

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/bloc/api_bloc/api_service_bloc.dart';
 import 'package:movies_app/bloc/navigation_bloc/navigation_bloc.dart';
+import 'package:movies_app/bloc/watchlist_bloc/watchlist_bloc.dart';
 import 'package:movies_app/constants/theme_constants.dart';
 import 'package:movies_app/repositories/movie_repository.dart';
 import 'package:movies_app/repositories/tv_show_repository.dart';
-import 'package:movies_app/service_locator.dart';
 import 'package:movies_app/screens/splash_screen/splash_screen.dart';
 import 'package:movies_app/services/isar_service.dart';
-import 'bloc/api_bloc/api_service_bloc.dart';
-import 'bloc/watchlist_bloc/watchlist_bloc.dart';
+import 'package:movies_app/utils/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   initServiceLocator();
   await serviceLocator<IsarService>().isarDBInit();
   runApp(
-    const MyApp(),
+    const MainApp(),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
